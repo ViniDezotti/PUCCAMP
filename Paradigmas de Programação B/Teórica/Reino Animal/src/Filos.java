1bdcad04e42dc2e8e62828f827c09e5f156ccc5f
Guilherme Azevedo <79539989+guiazevedo17@users.noreply.github.com>
2021-09-13 17:33:50 -0300

    Add files via upload

---
+++ Paradigmas de Programação B/Teórica/Reino Animal/src/Filos.java
@@ -0,0 +1,23 @@
+public class Filos {
+    
+    public class Cordados {
+
+        public class Mamiferos {
+
+            public class Terrestre {
+
+            }
+
+            public class Aereo {
+
+            }
+
+            public class Marinho {
+                
+            }
+
+        }
+
+    }
+
+}
